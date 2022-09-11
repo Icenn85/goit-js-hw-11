83763781031894231a09a76ba397258fa27b9a29
Anastasiia Yavtushenko <Icenn85@gmail.com>
2022-09-11 21:47:47 +0300

    add corrections

--- src/index.js
+++ src/index.js
@@ -26,18 +26,31 @@ function imageMarkup(images) {
         comments,
         downloads,
       } = image;
-      return `
-        <a class="gallery__link" href="${largeImageURL}">
-          <div class="gallery-item" id="${id}">
+        return `
+      <div class="photo-card">
+        <a href="${largeImageURL}">
             <img class="gallery-item__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            </a>
             <div class="info">
-              <p class="info-item"><b>Likes</b>${likes}</p>
-              <p class="info-item"><b>Views</b>${views}</p>
-              <p class="info-item"><b>Comments</b>${comments}</p>
-              <p class="info-item"><b>Downloads</b>${downloads}</p>
-            </div>
-          </div>
-        </a>
+                <div class="info-item-container">
+                <p class="info-item"><b>Likes</b></p>
+                <p>${likes}</p>
+                </div>
+             
+        <div class="info-item-container">
+        <p class="info-item"><b>Views</b></p>
+        <p>  ${views}</p>
+        </div>
+                <div class="info-item-container">
+                <p class="info-item"><b>Comments</b></p>
+                <p>${comments}</p>
+                </div>
+        <div class="info-item-container">
+        <p class="info-item"><b>Downloads</b></p>
+        <p>${downloads}</p>
+        </div>
+        </div>
+        </div>
       `;
     })
     .join('');
